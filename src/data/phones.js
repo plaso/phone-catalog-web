aa0962f787ddc1be5e99e04dd7037495cca7c4df
Pablo Laso <plasocortabitarte@gmail.com>
2018-05-21 10:01:50 +0200

    feat: deploy changes

--- src/data/phones.js
+++ src/data/phones.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000/phones';
+const API_URL = 'https://phone-catalog.herokuapp.com/';
 
 const getPhones = () => {
   return axios.get(API_URL)
