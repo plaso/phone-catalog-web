d76a3488609cb4c2e86648acbd685816caa1ce69
Pablo Laso <plasocortabitarte@gmail.com>
2018-05-21 10:02:59 +0200

    fix: api url

--- src/data/phones.js
+++ src/data/phones.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://phone-catalog.herokuapp.com/';
+const API_URL = 'https://phone-catalog.herokuapp.com/phone';
 
 const getPhones = () => {
   return axios.get(API_URL)
